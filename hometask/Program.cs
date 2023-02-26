1558d7f603318d645d6647b248e17d4d2b981cea
Elgun Gafarzade <elgungafarzade@Elgun-MacBook-Air.local>
2023-02-26 23:22:58 +0400

    3 task var burda

---
+++ hometask/Program.cs
@@ -0,0 +1,90 @@
+﻿#region Imtahan bali
+
+//Console.WriteLine("Imtahan neticenizi daxil edin :");
+
+//int bal = int.Parse(Console.ReadLine());
+
+//if (bal > 100)
+//{
+//    Console.WriteLine("Bal 0-100 araliginda olur");
+//}
+//else if (bal <0)
+//{
+//    Console.WriteLine("Bal 0-100 araliginda olur");
+//}
+//else if (bal <= 50&&bal>=0) 
+//{
+
+//    Console.WriteLine("Kesildiniz");
+
+//}
+
+//else if (bal>50&&bal<=60) 
+//{
+
+//    Console.WriteLine("Imtahan neticeniz : E");
+
+//}
+//else if (bal > 60 && bal <= 70) 
+//{
+
+//    Console.WriteLine("Imtahan neticeniz : D");
+
+//}
+//else if (bal > 70 && bal <= 80)
+//{
+
+//    Console.WriteLine("Imtahan neticeniz : C");
+
+//}
+//else if (bal > 80 && bal <= 90)
+//{
+
+//    Console.WriteLine("Imtahan neticeniz : B");
+
+//}
+//else if (bal > 90 && bal <= 100)
+//{
+
+//    Console.WriteLine("Imtahan neticeniz : D");
+
+//}
+#endregion
+#region menfi musbet ededlerin sayi
+//int[] array = { 4, -5, 6, 8, -3, 9, -21, 31,24 };
+
+//int menfisayi = 0;
+//int musbetsayi = 0;
+
+//foreach (int i in array)
+//{
+//    if (i<0)
+//    {
+//        menfisayi++;
+
+//    }
+//    else if(i>0)
+//    {
+
+//        musbetsayi++;
+//    }
+//}
+
+//Console.WriteLine("Menfi ededlerin sayi :" + menfisayi);
+//Console.WriteLine("Musbet ededlerin sayi :"+ musbetsayi);
+#endregion
+#region en boyuk ededin tapilmasi
+//int[] array = { 34, 55, 32, 1, 3, 5, 2, 6,98};
+
+//int enboyuk = 0;
+
+//foreach (int i in array)
+//{
+//if(i>enboyuk)
+//    {
+
+//        enboyuk = i;
+//    }
+//}
+//Console.WriteLine(enboyuk);
+#endregion
