032fd48607c1c382295ecb1c7db9b734a0366f95
Elgun Gafarzade <elgungafarzade@Elgun-MacBook-Air.local>
2023-02-28 11:46:36 +0400

    27 february hometask

--- hometask/Program.cs
+++ hometask/Program.cs
@@ -1,90 +1,36 @@
-﻿#region Imtahan bali
+﻿#region task1
+//int[] arr = { 4, 7, 8, 9, 23, 66 };
+//int num = 9;
+//Axtar(arr, num);
 
-//Console.WriteLine("Imtahan neticenizi daxil edin :");
-
-//int bal = int.Parse(Console.ReadLine());
-
-//if (bal > 100)
-//{
-//    Console.WriteLine("Bal 0-100 araliginda olur");
-//}
-//else if (bal <0)
-//{
-//    Console.WriteLine("Bal 0-100 araliginda olur");
-//}
-//else if (bal <= 50&&bal>=0) 
+//void Axtar(int[] arr,int num)
 //{
 
-//    Console.WriteLine("Kesildiniz");
-
-//}
-
-//else if (bal>50&&bal<=60) 
-//{
-
-//    Console.WriteLine("Imtahan neticeniz : E");
-
-//}
-//else if (bal > 60 && bal <= 70) 
-//{
-
-//    Console.WriteLine("Imtahan neticeniz : D");
-
-//}
-//else if (bal > 70 && bal <= 80)
-//{
-
-//    Console.WriteLine("Imtahan neticeniz : C");
-
-//}
-//else if (bal > 80 && bal <= 90)
-//{
-
-//    Console.WriteLine("Imtahan neticeniz : B");
-
-//}
-//else if (bal > 90 && bal <= 100)
-//{
-
-//    Console.WriteLine("Imtahan neticeniz : D");
-
+//    foreach (var i in arr)
+//    {
+//        if(i==num)
+//        {
+//            Console.WriteLine("var");
+//            break;
+//        }
+//    }
 //}
 #endregion
-#region menfi musbet ededlerin sayi
-//int[] array = { 4, -5, 6, 8, -3, 9, -21, 31,24 };
 
-//int menfisayi = 0;
-//int musbetsayi = 0;
+#region task2
 
-//foreach (int i in array)
+//int Enboyuk(params int[] arr);
+
 //{
-//    if (i<0)
+//    int Max = 0;
+//    foreach (int number in arr)
 //    {
-//        menfisayi++;
-
+//        if (number>Max)
+//        {
+//            Max = number;
+//        }
 //    }
-//    else if(i>0)
-//    {
 
-//        musbetsayi++;
-//    }
+//    return Max;
 //}
-
-//Console.WriteLine("Menfi ededlerin sayi :" + menfisayi);
-//Console.WriteLine("Musbet ededlerin sayi :"+ musbetsayi);
-#endregion
-#region en boyuk ededin tapilmasi
-//int[] array = { 34, 55, 32, 1, 3, 5, 2, 6,98};
-
-//int enboyuk = 0;
-
-//foreach (int i in array)
-//{
-//if(i>enboyuk)
-//    {
-
-//        enboyuk = i;
-//    }
-//}
-//Console.WriteLine(enboyuk);
 #endregion
